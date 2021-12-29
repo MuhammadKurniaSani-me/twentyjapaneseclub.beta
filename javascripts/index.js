a47e1e278c32d7f3bfb6781966c62060e1bf9101
Muhammad Kurnia Sani <sani230402@gmail.com>
2021-12-29 17:37:10 +0700

    fixed, close navbar without toggle button

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,6 +1,7 @@
+const navbarlinks = document.getElementsByClassName("navbar-links")[0];
+
 function hamburgerMenu() {
-    const toggleButton = document.getElementsByClassName("toggle-button")[0];
-    const navbarlinks = document.getElementsByClassName("navbar-links")[0];
+    const toggleButton = document.getElementsByClassName("toggle-button")[0];    
 
     toggleButton.addEventListener("click", ()=> {
         navbarlinks.classList.toggle("active");
@@ -8,5 +9,16 @@ function hamburgerMenu() {
     
 }
 
+function closeHamburgerMenu() {
+    const navbarMenu = document.querySelector(".navbar-links").classList;
+    const main = document.querySelector("main")
+    main.addEventListener("click", ()=> {
+        if (navbarMenu.contains("active")) {
+            navbarlinks.classList.remove("active");
+        }
+    });
+}
+
 // Run Function
 hamburgerMenu();
+closeHamburgerMenu();
